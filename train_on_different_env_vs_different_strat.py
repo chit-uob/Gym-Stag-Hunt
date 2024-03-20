3ac7c83c0855c6419d1c09658c174eb49f027de2
Chit <cxl203@student.bham.ac.uk>
2024-03-20 10:34:44 +0000

    train added reward

--- train_on_different_env_vs_different_strat.py
+++ train_on_different_env_vs_different_strat.py
@@ -9,11 +9,11 @@ TYPE_OF_SETTINGS_NAMES = ["always_go_to_stag", "always_go_to_plant", "tit_for_ta
 for env_type in TYPE_OF_ENVS:
     for setting_type, settings_name in zip(TYPE_OF_SETTINGS, TYPE_OF_SETTINGS_NAMES):
         rl_agent = EpsilonGreedyQLAgent('player_1', 5, epsilon=0.9, alpha=0.1, gamma=0.9)
-        file_prefix = f"training_results/{env_type.__name__}_{settings_name}_"
+        file_prefix = f"training_results/{env_type.__name__}_{settings_name}_added_reward_"
         print(file_prefix)
         train_agent(env_type, setting_type, rl_agent,
                     need_eval=True, rl_agent_filename=file_prefix+"table.pkl",
                     eval_result_filename=file_prefix+"result.json")
         print("finished training")
         plot_eval_results(file_prefix+"result.json", file_prefix[17:])
-        play_agent(env_type, setting_type, file_prefix+"table.pkl", load_renderer=False)
+        # play_agent(env_type, setting_type, file_prefix+"table.pkl", load_renderer=False)
